6e51d7766dfbd223f444cdb4e219b5253a0d6c6b
mayudev <99561108+mayudev@users.noreply.github.com>
2023-03-08 16:47:47 +0100

    chore: bootstrap

--- src/controls/AdwButton.ts
+++ src/controls/AdwButton.ts
@@ -1,12 +1,35 @@
-import { css, html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { html, LitElement, unsafeCSS } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
+
+import style from "../styles/buttons.scss?inline";
 
 @customElement("adw-button")
 export default class AdwButton extends LitElement {
-  static styles = css``;
+  static styles = unsafeCSS(style);
+
+  @property({ type: Boolean })
+  primary: boolean = false;
+
+  @property({ type: Boolean })
+  circular: boolean = false;
+
+  @property({ type: Boolean })
+  danger: boolean = false;
+
+  @property({ type: Boolean })
+  pill: boolean = false;
 
   protected render() {
-    return html`<button>
+    const classes = {
+      "adw-button": true,
+      "adw-button-primary": this.primary,
+      "adw-button-circular": this.circular,
+      "adw-button-danger": this.danger,
+      "adw-button-pill": this.pill,
+    };
+
+    return html`<button class=${classMap(classes)}>
       <slot />
     </button>`;
   }
--- src/stories/Buttons.stories.ts
+++ src/stories/Buttons.stories.ts
@@ -2,7 +2,33 @@ import "../controls/AdwButton.ts";
 import { html } from "lit-html";
 
 export default {
-  title: "Button",
+  title: "Components/Actions/Button",
+  component: "adw-button",
+  argTypes: {
+    primary: Boolean,
+    danger: Boolean,
+  },
+  args: {
+    primary: false,
+    danger: false,
+  },
 };
 
-export const Primary = () => html`<adw-button>button</adw-button>`;
+const Template = ({ primary, danger }: any) =>
+  html`<div>
+    <adw-button .primary=${primary} .danger=${danger}>Button</adw-button>
+
+    <adw-button .primary=${primary} .danger=${danger} pill>Button</adw-button>
+
+    <adw-button .primary=${primary} .danger=${danger} circular>
+      <span class="material-icons">add</span>
+    </adw-button>
+  </div>`;
+
+export const Default = Template.bind({});
+
+export const Primary = Template.bind({});
+Primary.args = { primary: true };
+
+export const Danger = Template.bind({});
+Danger.args = { danger: true };
