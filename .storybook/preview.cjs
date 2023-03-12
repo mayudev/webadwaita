a5dd03968ee87afa1af216a76e6715740bc6b5da
mayudev <99561108+mayudev@users.noreply.github.com>
2023-03-12 16:12:57 +0100

    feat: add a storybook theme

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,5 +1,13 @@
 import { themes } from "@storybook/theming";
 
+const create = (theme) => {
+  return {
+    ...theme,
+    brandImage:
+      "https://gitlab.gnome.org/uploads/-/system/project/avatar/14079/libadwaita.png?width=32",
+  };
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -10,8 +18,16 @@ export const parameters = {
   },
   darkMode: {
     classTarget: "html",
-    dark: { ...themes.dark, appBg: "#303030", appContentBg: "#242424" },
-    light: { ...themes.normal, appBg: "#ebebeb", appContentBg: "#fafafa" },
+    dark: {
+      ...create(themes.dark),
+      appBg: "#303030",
+      appContentBg: "#242424",
+    },
+    light: {
+      ...create(themes.normal),
+      appBg: "#ebebeb",
+      appContentBg: "#fafafa",
+    },
     darkClass: "adw-dark",
     lightClass: "adw-default",
     stylePreview: true,
